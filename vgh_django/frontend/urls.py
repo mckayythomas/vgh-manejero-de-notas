5300685e3deb68e0adfe43ae22a17c0673256b9a
mckayythomas <mckayycollins@gmail.com>
2023-10-02 21:15:34 -0500

    Frontend dashboards complete

--- vgh_django/frontend/urls.py
+++ vgh_django/frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from frontend.views import render_profesor_dashboard
+from frontend.views import render_profesor_dashboard, render_tutor_dashboard, render_director_dashboard
 
 urlpatterns = [
-    path('profesor/', render_profesor_dashboard, name='profesor-dashboard')
+    path('profesor/', render_profesor_dashboard, name='profesor-dashboard'),
+    path('tutor/', render_tutor_dashboard, name='tutor-dashboard'),
+    path('director/', render_director_dashboard, name='director-dashboard')
 ]
--- vgh_django/frontend/views.py
+++ vgh_django/frontend/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def render_profesor_dashboard(request):
     return render(request, 'profesor-dashboard.html')
+
+def render_tutor_dashboard(request):
+    return render(request, 'tutoria-dashboard.html')
+
+def render_director_dashboard(request):
+    return render(request, 'director-dashboard.html')
